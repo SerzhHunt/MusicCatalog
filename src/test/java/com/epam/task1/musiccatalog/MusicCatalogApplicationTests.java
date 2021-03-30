8ec042748bde3866aaff314a75f458a93f5f5085
Sergey Rakov <rakovsergey94@gmail.com>
2021-03-30 19:20:02 +0300

    Initial commit

---
+++ src/test/java/com/epam/task1/musiccatalog/MusicCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epam.task1.musiccatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicCatalogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
