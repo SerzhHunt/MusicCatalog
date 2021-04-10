c24113bd3bd4da7516acd3ec74f3bd33fd7f3f2e
Sergey Rakov <rakovsergey94@gmail.com>
2021-04-10 23:58:51 +0300

    add service layer

--- src/main/java/com/epam/musiccatalog/service/impl/AlbumServiceImpl.java
+++ src/main/java/com/epam/musiccatalog/service/impl/AlbumServiceImpl.java
@@ -1,21 +1,27 @@
 package com.epam.musiccatalog.service.impl;
 
-import com.epam.musiccatalog.dto.AuthorDto;
+import com.epam.musiccatalog.exception.album.AlbumConverterException;
+import com.epam.musiccatalog.exception.album.AlbumException;
+import com.epam.musiccatalog.exception.album.AlbumNotFoundException;
 import com.epam.musiccatalog.model.Album;
 import com.epam.musiccatalog.dto.AlbumDto;
-import com.epam.musiccatalog.model.Author;
 import com.epam.musiccatalog.model.Song;
 import com.epam.musiccatalog.repository.AlbumRepository;
 import com.epam.musiccatalog.service.AlbumService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import ma.glasnost.orika.MapperFacade;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.time.Duration;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class AlbumServiceImpl implements AlbumService {
@@ -23,61 +29,122 @@ public class AlbumServiceImpl implements AlbumService {
     private final MapperFacade mapper;
 
     @Override
-    public List<AlbumDto> getAll() {
-        return albumRepository.findAll()
-                .stream()
-                .map(this::albumToDto)
-                .collect(Collectors.toList());
-    }
-
-    @Override
-    public AlbumDto getAlbumById(Long id) {
-        Optional<Album> album = albumRepository.findById(id);
-
-        if (album.isEmpty()) {
-            throw new RuntimeException(); //TODO AlbumNotFoundException
-//            AlbumDto albumDto = mapper.map(album, AlbumDto.class);
-//
-//            albumDto.setDuration(Duration.ofMinutes(album.get().getSongs()
-//                    .stream()
-//                    .map(Song::getDuration)
-//                    .count()));
-//
-//            albumDto.setSongs(album.get().getSongs()
-//                    .stream()
-//                    .map(Song::getName)
-//                    .collect(Collectors.toList()));
-//
-//            albumDto.setAuthors(album.get().getSongs()
-//                    .stream()
-//                    .map(s -> s.getAuthor().getFirstname().concat(" " + s.getAuthor().getLastname()))
-//                    .collect(Collectors.toList()));
-//
-//            return albumDto;
+    @Transactional
+    public List<AlbumDto> getAll() throws AlbumException {
+        try {
+            List<AlbumDto> albums = new ArrayList<>();
+            for (Album album : albumRepository.findAll()) {
+                albums.add(albumToDto(album));
+            }
+            return albums;
+        } catch (Exception e) {
+            String message = "exception while getting all albums";
+            log.error(message);
+            throw new AlbumException(message, e);
         }
-        return albumToDto(album.get());
     }
 
     @Override
-    public AlbumDto save(AlbumDto albumDto) {
-        return null;
+    @Transactional
+    public AlbumDto getAlbumById(Long id) throws AlbumException {
+        try {
+            Optional<Album> album = albumRepository.findById(id);
+
+            if (album.isEmpty()) {
+                log.error("author not found by id {} ...", id);
+                throw new AlbumNotFoundException(id);
+            }
+            return albumToDto(album.get());
+        } catch (Exception e) {
+            String message = "exception while getting album from db";
+            log.error(message, e.getMessage());
+            throw new AlbumException(message, e);
+        }
     }
 
     @Override
-    public AlbumDto update(Long id, AlbumDto albumDto) {
-        return null;
+    @Transactional
+    public AlbumDto save(AlbumDto albumDto) throws AlbumException {
+        try {
+            Album savedAlbum = albumRepository.save(dtoToAuthor(albumDto));
+            return mapper.map(savedAlbum, AlbumDto.class);
+        } catch (Exception e) {
+            String message = "exception while save album in db";
+            log.error(message, e.getMessage());
+            throw new AlbumException(message, e);
+        }
     }
 
     @Override
-    public void delete(Long id) {
+    @Transactional
+    public AlbumDto update(Long id, AlbumDto albumDto) throws AlbumException {
+        try {
+            Optional<Album> albumById = albumRepository.findById(id);
 
+            if (albumById.isEmpty()) {
+                log.error("album not found by id {} ...", id);
+                throw new AlbumNotFoundException(id);
+            }
+            Album savedAlbum = albumRepository.save(updateAlbum(albumById.get(), albumDto));
+            return albumToDto(savedAlbum);
+        } catch (Exception e) {
+            String message = "exception while update album in db";
+            log.error(message, e.getMessage());
+            throw new AlbumException(message, e);
+        }
     }
 
-    private AlbumDto albumToDto(Album album) {
-        AlbumDto albumDto = mapper.map(album, AlbumDto.class);
-        albumDto.setDuration(getSumSongsDurationFromAlbum(album));
-        albumDto.setSongNames(getSongNamesOfAlbum(album));
-        return albumDto;
+    @Override
+    @Transactional
+    public void delete(Long id) throws AlbumException {
+        try {
+            albumRepository.deleteById(id);
+        } catch (Exception e) {
+            String message = "exception while delete album in db";
+            log.error(message, e.getMessage());
+            throw new AlbumException(message, e);
+        }
+    }
+
+    private AlbumDto albumToDto(Album album) throws AlbumConverterException {
+        if (Objects.isNull(album)) {
+            throw new AlbumConverterException();
+        }
+        try {
+            AlbumDto albumDto = mapper.map(album, AlbumDto.class);
+            albumDto.setDuration(getSumSongsDurationFromAlbum(album));
+            albumDto.setSongNames(getSongNamesOfAlbum(album));
+            return albumDto;
+        } catch (Exception e) {
+            String message = "error occurred during converting from entity to dto";
+            log.error(message, e.getMessage());
+            throw new AlbumConverterException(message, e);
+        }
+    }
+
+    private Album dtoToAuthor(AlbumDto albumDto) throws AlbumConverterException {
+        if (Objects.isNull(albumDto)) {
+            throw new AlbumConverterException();
+        }
+        try {
+            return mapper.map(albumDto, Album.class);
+        } catch (Exception e) {
+            String message = "error occurred during converting from dto to entity";
+            log.error(message, e.getMessage());
+            throw new AlbumConverterException(message, e);
+        }
+    }
+
+    private Album updateAlbum(Album album, AlbumDto albumDto) throws AlbumException {
+        if (Objects.isNull(albumDto)) {
+            String message = "album dto is null";
+            log.error(message);
+            throw new AlbumException(message);
+        }
+        album.setId(albumDto.getId());
+        album.setName(albumDto.getName());
+        album.setCreatedDate(albumDto.getCreatedDate());
+        return album;
     }
 
     private Duration getSumSongsDurationFromAlbum(Album album) {
--- src/main/java/com/epam/musiccatalog/service/impl/AuthorServiceImpl.java
+++ src/main/java/com/epam/musiccatalog/service/impl/AuthorServiceImpl.java
@@ -1,58 +1,124 @@
 package com.epam.musiccatalog.service.impl;
 
+import com.epam.musiccatalog.exception.author.AuthorConvertingException;
+import com.epam.musiccatalog.exception.author.AuthorException;
+import com.epam.musiccatalog.exception.author.AuthorNotFoundException;
 import com.epam.musiccatalog.model.Author;
 import com.epam.musiccatalog.model.Song;
 import com.epam.musiccatalog.dto.AuthorDto;
 import com.epam.musiccatalog.repository.AuthorRepository;
 import com.epam.musiccatalog.service.AuthorService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import ma.glasnost.orika.MapperFacade;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+@Slf4j
 @Service("authorService")
 @RequiredArgsConstructor
-@Transactional
 public class AuthorServiceImpl implements AuthorService {
     private final AuthorRepository authorRepository;
     private final MapperFacade mapper;
 
     @Override
-    public List<AuthorDto> getAll() {
-        return null;
-    }
-
-    @Override
-    public AuthorDto getAuthorById(Long id) {
-        Optional<Author> author = authorRepository.findById(id);
-        if (author.isEmpty()) {
-            throw new RuntimeException(); //TODO authorNotFoundException
+    @Transactional
+    public List<AuthorDto> getAll() throws AuthorException {
+        try {
+            List<AuthorDto> authors = new ArrayList<>();
+            for (Author author : authorRepository.findAll()) {
+                AuthorDto authorDto = authorToDto(author);
+                authors.add(authorDto);
+            }
+            return authors;
+        } catch (Exception e) {
+            String message = "exception while getting all authors";
+            log.error(message);
+            throw new AuthorException(message, e);
         }
-        return authorToDto(author.get());
     }
 
     @Override
-    public AuthorDto save(AuthorDto authorDto) {
-        return null;
+    @Transactional
+    public AuthorDto getAuthorById(Long id) throws AuthorException {
+        try {
+            Optional<Author> author = authorRepository.findById(id);
+
+            if (author.isEmpty()) {
+                log.error("author not found by id {} ...", id);
+                throw new AuthorNotFoundException(id);
+            }
+            return authorToDto(author.get());
+        } catch (Exception e) {
+            String message = "exception while getting author from db";
+            log.error(message, e.getMessage());
+            throw new AuthorException(message, e);
+        }
     }
 
     @Override
-    public AuthorDto update(Long id, AuthorDto authorDto) {
-        return null;
+    @Transactional
+    public AuthorDto save(AuthorDto authorDto) throws AuthorException {
+        try {
+            Author savedAuthor = authorRepository.save(dtoToAuthor(authorDto));
+            return authorToDto(savedAuthor);
+        } catch (Exception e) {
+            String message = "exception while save author in db";
+            log.error(message, e.getMessage());
+            throw new AuthorException(message, e);
+        }
     }
 
     @Override
-    public void delete(Long id) {
+    @Transactional
+    public AuthorDto update(Long id, AuthorDto authorDto) throws AuthorException {
+        try {
+            Optional<Author> authorById = authorRepository.findById(id);
+            if (authorById.isEmpty()) {
+                log.error("author not found by id {} ...", id);
+                throw new AuthorNotFoundException(id);
+            }
+            Author savedAuthor = authorRepository.save(updateAuthor(authorById.get(), authorDto));
+            return authorToDto(savedAuthor);
+        } catch (Exception e) {
+            String message = "exception while update author in db";
+            log.error(message, e.getMessage());
+            throw new AuthorException(message, e);
+        }
+
     }
 
-    private AuthorDto authorToDto(Author author) {
-        AuthorDto authorDto = mapper.map(author, AuthorDto.class);
-        authorDto.setSongNames(getSongNamesOfAuthor(author));
-        return authorDto;
+    @Override
+    @Transactional
+    public void delete(Long id) throws AuthorException {
+        try {
+            authorRepository.deleteById(id);
+        } catch (Exception e) {
+            String message = "exception while delete author in db";
+            log.error(message, e.getMessage());
+            throw new AuthorException(message, e);
+        }
+    }
+
+    private AuthorDto authorToDto(Author author) throws AuthorConvertingException {
+        if (Objects.isNull(author)) {
+            throw new AuthorConvertingException();
+        }
+        try {
+            AuthorDto authorDto = mapper.map(author, AuthorDto.class);
+            authorDto.setSongNames(getSongNamesOfAuthor(author));
+            return authorDto;
+        } catch (Exception e) {
+            String message = "error occurred during converting from entity to dto";
+            log.error(message, e.getMessage());
+            throw new AuthorConvertingException(message, e);
+        }
     }
 
     private List<String> getSongNamesOfAuthor(Author author) {
@@ -60,5 +126,31 @@ public class AuthorServiceImpl implements AuthorService {
                 .stream()
                 .map(Song::getName)
                 .collect(Collectors.toList());
+    }
+
+    private Author dtoToAuthor(AuthorDto authorDto) throws AuthorConvertingException {
+        if (Objects.isNull(authorDto)) {
+            throw new AuthorConvertingException();
+        }
+        try {
+            return mapper.map(authorDto, Author.class);
+        } catch (Exception e) {
+            String message = "error occurred during converting from dto to entity";
+            log.error(message, e.getMessage());
+            throw new AuthorConvertingException(message, e);
+        }
+    }
+
+    private Author updateAuthor(Author author, AuthorDto authorDto) throws AuthorException {
+        if (Objects.isNull(authorDto)) {
+            String message = "author dto is null";
+            log.error(message);
+            throw new AuthorException(message);
+        }
+        author.setId(author.getId());
+        author.setFirstname(authorDto.getFirstname());
+        author.setLastname(author.getLastname());
+        author.setBirthDate(authorDto.getBirthDate());
+        return author;
     }
 }
