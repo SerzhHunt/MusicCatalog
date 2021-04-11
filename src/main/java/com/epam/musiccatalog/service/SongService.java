ac12e0cad2de5469c4dc5f6d253b176d18ae4813
Sergey Rakov <rakovsergey94@gmail.com>
2021-04-11 19:39:42 +0300

    add song service layer

--- src/main/java/com/epam/musiccatalog/service/SongService.java
+++ src/main/java/com/epam/musiccatalog/service/SongService.java
@@ -1,18 +1,19 @@
 package com.epam.musiccatalog.service;
 
 import com.epam.musiccatalog.dto.SongDto;
+import com.epam.musiccatalog.exception.song.SongException;
 
 import java.util.List;
 
 public interface SongService {
 
-    List<SongDto> getAll();
+    List<SongDto> getAll() throws SongException;
 
-    SongDto getSongById(Long albumId, Long songId);
+    SongDto getSongById(Long albumId, Long songId) throws SongException;
 
-    SongDto save(Long albumId, SongDto songDto);
+    SongDto save(Long albumId, SongDto songDto) throws SongException;
 
-    SongDto update(Long albumId, SongDto songDto);
+    SongDto update(Long songId, SongDto songDto) throws SongException;
 
-    void delete(Long albumId, Long songId);
+    void delete(Long songId) throws SongException;
 }
