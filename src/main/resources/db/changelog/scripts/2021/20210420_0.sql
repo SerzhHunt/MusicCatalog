8ad58ad5ee7d36c70fdefbf00030706e0cb5ba5f
Sergey Rakov <rakovsergey94@gmail.com>
2021-04-20 23:10:43 +0300

    add dataset

---
+++ src/main/resources/db/changelog/scripts/2021/20210420_0.sql
@@ -0,0 +1,18 @@
+INSERT INTO author (first_name, last_name, birth_date)
+VALUES ('Metallica', '', '1970/01/01');
+INSERT INTO author (first_name, last_name, birth_date)
+VALUES ('Megadeth', '', '1970/01/01');
+INSERT INTO author (first_name, last_name, birth_date)
+VALUES ('Anthrax', '', '1970/01/01');
+INSERT INTO author (first_name, last_name, birth_date)
+VALUES ('Eric Clapton', '', '1970/01/01');
+INSERT INTO author (first_name, last_name, birth_date)
+VALUES ('ZZ Top', '', '1970/01/01');
+INSERT INTO author (first_name, last_name, birth_date)
+VALUES ('Van Halen', '', '1970/01/01');
+INSERT INTO author (first_name, last_name, birth_date)
+VALUES ('Lynyrd Skynyrd', '', '1970/01/01');
+INSERT INTO author (first_name, last_name, birth_date)
+VALUES ('AC/DC', '', '1970/01/01');
+INSERT INTO author (first_name, last_name, birth_date)
+VALUES ('The Beatles', '', '1970/01/01');
